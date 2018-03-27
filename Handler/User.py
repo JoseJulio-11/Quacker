f076a832f1c6206f3229952d36930619ceb28721
josejulio11 <jose.sanchez25@upr.edu>
2018-03-27 16:18:37 -0700

    Minor change

--- Handler/User.py
+++ Handler/User.py
@@ -4,7 +4,7 @@ from Handler.DictionaryBuilder import DictionaryBuilder
 class UserHandler:
 
     def getAllUsers(self):
-        dao = ReadUserDAO()
+        dao = UserDAO()
         result = dao.getAllUsers()
         mapped_result = []
         for r in result:
