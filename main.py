75c7ca111ef061424945f28ce3064c55b92f1da4
alberto64 <alberto.dejesus@upr.edu>
2018-03-27 19:13:51 -0400

    Played with main

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from flask import Flask, jsonify, request
-from DAO import UserDAO
-from Handler import Chat, Dashboard, Message, User, DictionaryBuilder
+#from Handler.Chat import Chat
+#from Handler.Message import Message
+#from Handler.User import User
+from Handler.DictionaryBuilder import DictionaryBuilder
 
 # Activate
 app = Flask(__name__)
 
 @app.route('/')
 def mainPage():
-    return 'Welcome, this is the main page for our project application Quacker!!!\n\n' \
-           'Currently you can navigate to the following addresses in the app:\n' \
-           '\t 1. ###'
+    return '<p><b>Welcome</b>, this is the main page for our project application <b>Quacker!!!</b></p>' \
+           '<p>Currently you can navigate to the following addresses in the app:</p>' \
+           '<ol><li> ###</li>' \
+           '</ol>'
 
-
-@app.route('/login/<int:uid>', methods=['GET'])
-def getUserCredential(uid):
+@app.route('/user', methods=['GET'])
+def getAllUsers():
     if request.method == 'GET':
-        result = DictionaryBuilder.DictionaryBuilder.build_credential_dict([1, "Jackhammer", "TheHammer32", "jackhammer1@gmail.com", "7873431298"])
+        result = DictionaryBuilder.build_credential_dict(DictionaryBuilder,[1, "Jackhammer", "TheHammer32", "jackhammer1@gmail.com", "7873431298"])
         return jsonify(userCredential=result)
     else:
         return jsonify(Error="Method not allowed"), 405
