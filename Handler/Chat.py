faa3963f0b46c2471b65786692dc9a569b5ba664
alejandra <alejandra.casanova@upr.edu>
2018-03-27 23:13:07 -0400

    fixed the class handler chat,message and user

--- Handler/Chat.py
+++ Handler/Chat.py
@@ -5,66 +5,64 @@ from DAO.MessagesDAO import MessagesDAO
 
 dao = ChatDAO()
 
-class Chat:
+def getAllChats(self):
+     #This method will return all the chats
 
-    def getAllChats(self):
-         #This method will return all the chats
+    chat_lists = dao.getAllChats()
+    result_list = []
+    for row in chat_lists:
+        result = Dic.build_chat_dic(row)
+        result_list.append(result)
+    return jsonify(Chat = result_list)
 
-        chat_lists = dao.getAllChats()
-        result_list = []
-        for row in chat_lists:
-            result = Dic.build_chat_dic(row)
-            result_list.append(result)
-        return jsonify(Chat = result_list)
+def getChatByID(self,cID):
+    #This method will return the determined chat by its ID
+    row = dao.getChatByID(cID)
+    if not row:
+        return jsonify(Error = " Chat not found"), 404
+    else:
+        chat = Dic.build_chat_dic(row)
+        return jsonify(Chat = chat)
 
-    def getChatByID(self,cID):
-        #This method will return the determined chat by its ID
-        row = dao.getChatByID(cID)
-        if not row:
-            return jsonify(Error = " Chat not found"), 404
-        else:
-            chat = Dic.build_chat_dic(row)
-            return jsonify(Chat = chat)
+def getParticipantsByChatID(self,cID):
+    #TTHis method returns the list of participants in a determined chat
 
-    def getParticipantsByChatID(self,cID):
-        #TTHis method returns the list of participants in a determined chat
+    chat_participants = dao.getChatParticipant()
+    result_list = []
+    for row in chat_participants:
+        result = Dic.build_particpants_chat(row)
+        result_list.append(result)
+    return jsonify(Participants = result_list)
 
-        chat_participants = dao.getChatParticipant()
-        result_list = []
-        for row in chat_participants:
-            result = Dic.build_particpants_chat(row)
-            result_list.append(result)
-        return jsonify(Participants = result_list)
+def getMessagesByChatID(self,cID):
+     #This method will return the messages in a determined  chat
 
-    def getMessagesByChatID(self,cID):
-         #This method will return the messages in a determined  chat
+    chat_messages = dao.getChatMessages(cID)
+    result_messages = []
+    for row in chat_messages:
+        result = Dic.build_chat_messages(row)
+        result_messages.append(result)
+    return jsonify(Messages = result_messages)
 
-        chat_messages = dao.getChatMessages(cID)
-        result_messages = []
-        for row in chat_messages:
-            result = Dic.build_chat_messages(row)
-            result_messages.append(result)
-        return jsonify(Messages = result_messages)
+def getChatByUserID(self,uID):
+    #This method will return the chats on which the user are part of
 
-    def getChatByUserID(self,uID):
-        #This method will return the chats on which the user are part of
+    chats = dao.getChatByUserID(uID)
+    result_list = []
+    for row in chats:
+        result = Dic.build_chat_dict(row)
+        result_list.append(result)
+    return jsonify(Chats = result_list)
 
-        chats = dao.getChatByUserID(uID)
-        result_list = []
-        for row in chats:
-            result = Dic.build_chat_dict(row)
-            result_list.append(result)
-        return jsonify(Chats = result_list)
+def getALlParticipants(self):
+    #THis method will return all the participants on the application
 
-    def getALlParticipants(self):
-        #THis method will return all the participants on the application
-
-        participants = dao.getAllParticipants()
-        result_list =[]
-        for row in participants:
-            result = Dic.build_participants_dict(row)
-            result_list.append(result)
-        return jsonify(Participants = result_list)
+    participants = dao.getAllParticipants()
+    result_list =[]
+    for row in participants:
+        result = Dic.build_participants_dict(row)
+        result_list.append(result)
+    return jsonify(Participants = result_list)
 
 
 #   def removeChatGroup(self,cID):
--- Handler/Message.py
+++ Handler/Message.py
@@ -2,20 +2,20 @@ from flask import jsonify
 import Handler.DictionaryBuilder as Dic
 from DAO.MessagesDAO import MessagesDAO
 dao = MessagesDAO()
-class Message():
-    def getMessagesByID(self, mID):
-    #This method return the message requested by its ID
-        row = dao.getMessageInfo(mID)
-        if not row:
-            return jsonify(Error = " Message not found"), 404
-        else:
-            message = Dic.build_message_dic(row)
-            return jsonify(Message = message)
-    def getReactionsByMessage(self,mID):
-        #THis method return the reaction of a determined message
-        row = dao.getMessageReaction(mID)
-        if not row:
-            return jsonify(Error = " Message does not contain reaction"), 404
-        else:
-            reaction = Dic.built_react_dic(mID)
-            return jsonify(Reaction = reaction)
+
+def getMessagesByID(self, mID):
+#This method return the message requested by its ID
+    row = dao.getMessageInfo(mID)
+    if not row:
+        return jsonify(Error = " Message not found"), 404
+    else:
+        message = Dic.build_message_dic(row)
+        return jsonify(Message = message)
+def getReactionsByMessage(self,mID):
+    #THis method return the reaction of a determined message
+    row = dao.getMessageReaction(mID)
+    if not row:
+        return jsonify(Error = " Message does not contain reaction"), 404
+    else:
+        reaction = Dic.built_react_dic(mID)
+        return jsonify(Reaction = reaction)
