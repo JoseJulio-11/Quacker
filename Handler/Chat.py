cfd1fde6976e0cffad7d7082f642e3c8a6334421
alberto64 <alberto.dejesus@upr.edu>
2018-03-28 14:34:55 -0400

    Revised Chat Handler

--- Handler/Chat.py
+++ Handler/Chat.py
@@ -6,76 +6,79 @@ dao = ChatDAO()
 
 
 def getAllChats():
-     #This method will return all the chats
+    # This method will return all the chats
 
     chat_lists = dao.getAllChats()
+    if not chat_lists:
+        return jsonify(Error="No Records Found")
     result_list = []
     for row in chat_lists:
         result = Dic.build_chat_dict(row)
         result_list.append(result)
-    return jsonify(Chat = result_list)
+    return jsonify(Chat=result_list)
 
 
 def getChatByID(cID):
-    #This method will return the determined chat by its ID
-    #TODO verify if the row returns a None
+    # This method will return the determined chat by its ID
     row = dao.getChatByID(cID)
     if not row:
-        return jsonify(Error = " Chat not found"), 404
-    else:
-        chat = Dic.build_chat_dict(row)
-        return jsonify(Chat = chat)
+        return jsonify(Error=" Chat not found"), 404
+    chat = Dic.build_chat_dict(row)
+    return jsonify(Chat=chat)
 
 
 def getParticipantsByChatID(cID):
-    #TTHis method returns the list of participants in a determined chat
-    #TODO verify if the chat_participants returns None
-    chat_participants = dao.getChatParticipant()
+    # This method returns the list of participants in a determined chat
+    chat_participants = dao.getChatParticipants(cID)
+    if not chat_participants:
+        return jsonify(Error="No Participants Found")
     result_list = []
     for row in chat_participants:
         result = Dic.build_participants_dict(row)
         result_list.append(result)
-    return jsonify(Participants = result_list)
+    return jsonify(Participants=result_list)
 
 
 def getMessagesByChatID(cID):
-     #This method will return the messages in a determined  chat
-    #TODO verify if the chat_messages return a None
+    # This method will return the messages in a determined  chat
     chat_messages = dao.getChatMessages(cID)
+    if not chat_messages:
+        return jsonify(Error="No Messages Found")
     result_messages = []
     for row in chat_messages:
         result = Dic.build_message_dict(row)
         result_messages.append(result)
-    return jsonify(Messages = result_messages)
+    return jsonify(Messages=result_messages)
 
 
 def getChatByUserID(uID):
-    #This method will return the chats on which the user are part of
-    #TODO verify if it returns a NONE
+    # This method will return the chats on which the user are part of
     chats = dao.getChatByUserID(uID)
-    if chats is not None:
-        result_list = []
-        for row in chats:
-            result = Dic.build_chat_dict(row)
-            result_list.append(result)
-        return jsonify(Chats = result_list)
-    else:
-        return jsonify(Error = "Not Record Found!")
+    if not chats:
+        return jsonify(Error="No Chats Found")
+    result_list = []
+    for row in chats:
+        result = Dic.build_chat_dict(row)
+        result_list.append(result)
+    return jsonify(Chats=result_list)
 
 
 def getALlParticipants():
-    #THis method will return all the participants on the application
-    #TODO verify if it returns a None
+    # This method will return all the participants on the application
     participants = dao.getAllParticipants()
-    result_list =[]
+    if not participants:
+        return jsonify(Error="No Participants Found")
+    result_list = []
     for row in participants:
         result = Dic.build_participants_dict(row)
         result_list.append(result)
-    return jsonify(Participants = result_list)
+    return jsonify(Participants=result_list)
 
 
 def getChatMediaByID(cid):
     media = dao.getChatMedia(cid)
+    if not media:
+        return jsonify(Error="No Media Found")
     result_list = []
     for row in media:
         result = Dic.build_media_dict(row)
@@ -83,7 +86,9 @@ def getChatMediaByID(cid):
     return jsonify(Media=result_list)
 
 def getChatTopicByID(cid):
-    media = dao.getChatTopic(cid)
+    media = dao.getChatTopics(cid)
+    if not media:
+        return jsonify(Error="No Media Found")
     result_list = []
     for row in media:
         result = Dic.build_media_dict(row)
